7328769940e7ae5e0bc73534004bbdf65518e066
Wesley Oliveira <fcowesleyosantos@gmail.com>
2020-04-05 16:36:21 -0300

    feat: Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <h1>Trackovid</h1>
+                <ul>
+                    <li>Home</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
--- src/mainRoutes.js
+++ src/mainRoutes.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import Home from './views/Home';
+
+//Dependencies
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+//Components
+import Header from './components/Header'
+
+//Views
+import Home from './views/Home';
 
 const MainRoutes = () => (
     <BrowserRouter>
+        <Header />
         <Routes>
             <Route path="/" element={<Home />}></Route>
         </Routes>
