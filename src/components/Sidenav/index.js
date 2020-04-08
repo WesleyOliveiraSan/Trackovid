72928ae6991ed01424329772c8807d87072fd779
Wesley Oliveira <fcowesleyosantos@gmail.com>
2020-04-08 14:15:55 -0300

    feat: add sidenav

---
+++ src/components/Sidenav/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Sidenav() {
+    return (
+        <nav>
+            <h1><Link to="/">Trackovid</Link></h1>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="#">About</Link></li>
+                <li><Link to="#">Contact</Link></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Sidenav
