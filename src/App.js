9e4b0af8b4e843571cebeaa97206cd1869547515
Wesley Oliveira <fcowesleyosantos@gmail.com>
2020-04-08 14:18:12 -0300

    fix: updated code structure | making App component the body instead of mainRoutes.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react'
+
+//Dependencies
 import './assets/styles/app.scss'
+import { BrowserRouter } from 'react-router-dom';
+
+//Components
+import Header from './components/Header'
+import Footer from './components/Footer';
+import Sidenav from './components/Sidenav';
+
+//Routes
 import MainRoutes from './mainRoutes'
 
 function App() {
   return (
-    <div>
-      <MainRoutes/>
-    </div>
+    <BrowserRouter>
+      <div className="row">
+        <aside className="col-lg-2">
+          <Sidenav />
+        </aside>
+        <main className="col-lg-10">
+          <Header />
+          <MainRoutes />
+          <Footer />
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/mainRoutes.js
+++ src/mainRoutes.js
@@ -1,23 +1,15 @@
 import React from 'react';
 
 //Dependencies
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-//Components
-import Header from './components/Header'
-import Footer from './components/Footer';
+import { Routes, Route } from 'react-router-dom';
 
 //Views
 import Home from './views/Home';
 
 const MainRoutes = () => (
-    <BrowserRouter>
-        <Header />
-        <Routes>
-            <Route path="/" element={<Home />}></Route>
-        </Routes>
-        <Footer/>
-    </BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Home />}></Route>
+    </Routes>
 )
 
 export default MainRoutes;
