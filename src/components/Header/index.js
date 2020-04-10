56d1d5dbb14b90fc6165ed1d4c748d1c7dca07e0
Wesley Oliveira <fcowesleyosantos@gmail.com>
2020-04-10 13:47:12 -0300

    fix: remove fontawesome, add github.png icon, update sidebar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,11 +3,14 @@ import React from 'react'
 //Dependencies
 import { Link } from 'react-router-dom';
 
+//Assets
+import github from '../../assets/images/github.png'
+
 function Header() {
     return (
         <header className="justify-content-between d-md-flex d-none">
             <Link to="/"><h1>Trackovid</h1></Link>
-            <i class="fas fa-github text-white"></i>
+            <a href="https://github.com/WesleyOliveiraSan/Trackovid" target="_blank" rel="noopener noreferrer" className="align-self-center" ><img src={github} className="rounded-circle" alt="github-icon" /></a>
         </header>
     )
 }
