bd2951fee1898b7294dcf1691ca02e07f3329217
Wesley Oliveira <fcowesleyosantos@gmail.com>
2020-04-13 14:18:48 -0300

    fix: remove Home component && add index.js only to Home folder

--- src/mainRoutes.js
+++ src/mainRoutes.js
@@ -5,10 +5,17 @@ import { Routes, Route } from 'react-router-dom';
 
 //Views
 import Home from './views/Home';
+import HomeMap from './views/HomeMap'
+import HomeBrazil from './views/HomeBrazil'
+import HomeStates from './views/HomeStates'
 
 const MainRoutes = () => (
     <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Home />}>
+            <Route path='mapa' element={<HomeMap />} />
+            <Route path='brasil' element={<HomeBrazil />} />
+            <Route path='estados' element={<HomeStates />} />
+        </Route>
     </Routes>
 )
 
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,2 +1,18 @@
-import Home from './Home';
-export default Home;
+import React from 'react'
+
+//Dependencies
+import { Outlet, Link } from 'react-router-dom';
+
+function Home() {
+
+    return (
+        <div className="p-5">
+            <Link to='mapa'>Mapa</Link>
+            <Link to='brasil'>Brasil</Link>
+            <Link to='estados'>Estados</Link>
+            <Outlet />
+        </div>
+    )
+}
+
+export default Home
