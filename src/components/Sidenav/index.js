55d529e26ab06e0268bbe729d7afd53cab1c0ef5
Wesley Oliveira <fcowesleyosantos@gmail.com>
2020-04-13 15:42:36 -0300

    feat: add sidenav active tracking feature

--- src/components/Sidenav/index.js
+++ src/components/Sidenav/index.js
@@ -11,24 +11,28 @@ import { Link } from 'react-router-dom'
 
 function Sidenav() {
 
-    const [Links, setLinks] = useState(true)
+    const [path, setPath] = useState(window.location.pathname)
+    const [links, setLinks] = useState(true)
 
     return (
         <nav>
             <div className="d-md-none d-flex justify-content-between">
                 <Link className="link font-weight-bold" to="/">Trackovid</Link>
-                <img className="menu" src={Menu} onClick={() => setLinks(!Links)} alt="close" />
+                <img className="menu" src={Menu} onClick={() => setLinks(!links)} alt="close" />
             </div>
             <div>
                 <img className="logo" src={Logo} alt="Mask Logo" />
                 <p className="desc">Aplicação para rastrear os casos de Covid-19 no Brasil</p>
-                <div className="nav-div d-md-flex d-none justify-content-between" onClick={() => setLinks(!Links)}>
+                <div className="nav-div d-md-flex d-none justify-content-between" onClick={() => setLinks(!links)}>
                     <p>Navigation</p>
-                    <img className="menu" src={Links ? On : Off} onClick={() => setLinks(!Links)} alt="close" />
+                    <img className="menu" src={links ? On : Off} onClick={() => setLinks(!links)} alt="close" />
                 </div>
             </div>
-            <div style={Links ? { display: 'block' } : { display: 'none' }}>
-                <Link className="link active" to="/">Mapa</Link>
+            <div style={links ? { display: 'block' } : { display: 'none' }}>
+                <Link className={`link ${path === '/' && 'active'}`} onClick={() => setPath('/')} to="/">Inicio</Link>
+                <Link className={`link ${path === '/mapa' && 'active'}`} onClick={() => setPath('/mapa')} to="mapa">Mapa</Link>
+                <Link className={`link ${path === '/brasil' && 'active'}`} onClick={() => setPath('/brasil')} to="brasil">Brasil</Link>
+                <Link className={`link ${path === '/estados' && 'active'}`} onClick={() => setPath('/estados')} to="estados">Estados</Link>
             </div>
         </nav>
     )
