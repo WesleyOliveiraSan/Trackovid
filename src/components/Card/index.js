d794ca0711813a0b0a7ccd88d2d095dfbe444833
Wesley Oliveira <fcowesleyosantos@gmail.com>
2020-04-14 17:57:48 -0300

    fix: minor fixes

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ title, content, chart, bgColor="white", color = "black" }) => (
     <div className="card card-trackovid">
         <div>
-            <div className="card-body" style={{ backgroundColor: bgColor, color: color }}>
+            <div className="card-body" style={{ backgroundColor: bgColor, color: bgColor === "white" ? color : "white" }}>
                 {chart && chart}
                 {content && <h5>{content}</h5>}
                 {title && <p>{title}</p>}
