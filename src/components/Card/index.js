5148e18b27416f757ac06a734a342c9274dbfb10
Wesley Oliveira <fcowesleyosantos@gmail.com>
2020-04-14 17:13:47 -0300

    fix: add card colors

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Card = ({ title, content }) => (
+const Card = ({ title, content, color = "white" }) => (
     <div className="card card-trackovid">
         <div>
-            <div className="card-body">
+            <div className="card-body" style={{backgroundColor: color,color: color === 'white' ? '#304153' : 'white'}}>
                 <h5>{content}</h5>
                 <p>{title}</p>
             </div>
